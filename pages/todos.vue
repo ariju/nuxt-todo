d7b1a027037ca19c58179c39877ce6782600164b
ariju <jun.0309ari@gmail.com>
2021-01-14 18:13:26 +0900

    todo追加機能

--- pages/todos.vue
+++ pages/todos.vue
@@ -1,13 +1,37 @@
 <template>
   <div>
-
+    <div class="form">
+      <!-- v-on:submitでボタンが押された時にaddメソッドを呼ぶ -->
+      <!-- preventはsubmitイベントによってページがリロードされない様にする -->
+      <!-- inputはタスク名の入力欄nameとデータバインディングしている -->
+      <form v-on:submit.prevent="add">
+        <input v-model="name">
+        <button>Add</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
+    data: function() {
+      return {
+        name: '',
+        done: false
+      }
+    },
+    // トゥドゥのデータを一時的に格納しておくデータを用意
+    // createdのタイミングでdispatchメソッドでtodos.jsのアクションの
+    // initを読み込んでfirebaseの初期化を行う
     created: function() {
       this.$store.dispatch('todos/init')
+    },
+    // addメソッドstoreのtodos.jsのアクションの呼び出し引数にtodoにつける名前を渡す
+    methods: {
+      add() {
+        this.$store.dispatch('todos/add', this.name)
+        this.name = ''
+      }
     }
   }
 </script>
