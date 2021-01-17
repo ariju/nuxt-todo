c048cb70276976cbae57b61567861a3115ad2c21
ariju <jun.0309ari@gmail.com>
2021-01-17 17:15:21 +0900

    日本時刻の表示

--- pages/todos.vue
+++ pages/todos.vue
@@ -5,12 +5,15 @@
     <ul>
       <li v-for="todo in todos" :key="todo.id">
         <!-- {{ todo }} -->
+        <!-- 完了、未完了のチェックBOX実装 -->
         <input 
         type="checkbox"
         v-bind:checks="todo.done"
         @change="toggle=(todo)">
-        
-        {{ todo.name }} {{ todo.created }}
+        <!-- 取消線の実装 -->
+        <span v-bind:class="{ done: todo.done }">
+          {{ todo.name }} {{ todo.created.toDate() | dateFilter}}
+        </span>
         <!-- データの削除機能 -->
         <button v-on:click="remove(todo.id)">x</button>
       </li>
@@ -28,6 +31,7 @@
 </template>
 
 <script>
+  import moment from 'moment'
   export default {
     data: function() {
       return {
@@ -58,6 +62,18 @@
       todos() {
         return this.$store.state.todos.todos
       }
+    },
+    filters: {
+      dateFilter: function(date) {
+        return moment(date).format('YYYY/MM/DD HH:mm:ss')
+      }
     }
   }
 </script>
+
+<style>
+/* 取消線を実装 */
+li > span.done {
+  text-decoration: line-through;
+}
+</style>
